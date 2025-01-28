9314ebf4429f455fa97ec84e69d18fbeb9d7aa0d
kalpana-1 <kalpana.tamatam@arithaconsulting.com>
2025-01-28 11:24:08 +0530

    Fixed compliance assessment issue in update project API

--- dao/project_dao.js
+++ dao/project_dao.js
@@ -209,12 +209,14 @@ const projectQuery = async (queryType, params = {}) => {
                               last_run = '${params.last_run != null ? params.last_run : null}',
                               mapping_standards = '${params.mapping_standards}',
                               summary_report = '${JSON.stringify(params.summary_report)}'
-                              ${params.complianceAssesment ? `, complianceAssesment = '${JSON.stringify(params.complianceAssesment)}'` : ''}
+                              ${params.complianceAssesment ? `, complianceAssesment = '${params.complianceAssesment}'` : ''}
                               ${params.history ? `, history = '${JSON.stringify(params.history)}'` : ''}
                               ${params.no_of_runs ? ` , no_of_runs = ${params.no_of_runs}` : ''}
                               ${params.standardUploaded != null ? ` , standardUploaded = '${params.standardUploaded}'` : ''}
                               ${params.base64 ? `, base64 = '${JSON.stringify(params.base64)}'` : ''}
                         `;
+
+        // ${params.complianceAssesment ? `, complianceAssesment = '${JSON.stringify(params.complianceAssesment)}'` : ''}
 
         // Conditionally append checkListResponse and chatResponse
         if (params.checkListResponse) {
